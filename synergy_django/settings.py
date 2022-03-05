c2abab75926d4ee5a0b9e98a3a07d5f5546ec78e
RyanYJOh <hiimryan@kakao.com>
2022-03-05 10:23:43 +0900

    first deploy to heroku

--- synergy_django/settings.py
+++ synergy_django/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+# DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 if DEBUG == True:
     ALLOWED_HOSTS = [
